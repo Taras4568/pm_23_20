28713221ae97c652427086461bc858dfd73c043f
Taras Protsiv <137615339+Taras4568@users.noreply.github.com>
2024-12-11 18:46:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,84 +1,84 @@
-import express from 'express'; 
-import cors from 'cors'; 
-const app = express(); 
-const port = 4000; 
- 
-app.use(cors()); 
- 
-app.get('/data', (res, res) => { 
-    res.json({ 
-        education: [ 
-            { 
-                institution: "UNIVERSITY NAME", 
-                years: "2018 - PRESENT", 
-                degree: "MASTER OF CREATIVEARTS", 
-                description: "Lorem ipsum dolor sit, amet consectetur<br> adipisicing elit. Fugiat, molestias provident." 
-            }, 
-            { 
-                institution: "UNIVERSITY NAME", 
-                years: "2015 - 2017", 
-                degree: "MASTER GRAPHIC & WEB DESIGHER", 
-                description: "Lorem ipsum dolor sit, amet consectetur<br> adipisicing elit. Fugiat, molestias provident." 
-            }, 
-            { 
-                institution: "UNIVERSITY NAME", 
-                years: "2010 - 2012", 
-                degree: "MASTER OF DESIGH", 
-                description: "Lorem ipsum dolor sit, amet consectetur<br> adipisicing elit. Fugiat, molestias provident." 
-            } ], 
-        jobList: [ 
-            { 
-                company: "COMPNY NAME", 
-                years: "2020 - Present", 
-                position: "WEB DESINGER", 
-                description: "Lorem ipsum dolor sit, amet consectetur<br> adipisicing elit. Fugiat, molestias provident." 
-            }, 
-            { 
-                company: "COMPNY NAME", 
-                years: "2015 - 2018", 
-                position: "Graphic Designer", 
-                description: "Lorem ipsum dolor sit, amet consectetur<br> adipisicing elit. Fugiat, molestias provident." 
-            }, 
-            { 
-                company: "COMPNY NAME", 
-                years: "2010 - 2015", 
-                position: "MARKETING MANAGER", 
-                description: "Lorem ipsum dolor sit, amet consectetur<br> adipisicing elit. Fugiat, molestias provident." 
-            } ], 
-        contact: [ 
-            { 
-                phone: "+1-856-822-2156", 
-                website: "www.yourwebsite.com yourinfo@email.com", 
-                address: "1173 Valley Street Camber, NJ 08102" 
-            }], 
-        skills: [ 
-            { name: "Microsoft Word", value: "90%" }, 
-            { name: "Adobe illustrator", value: "65%" }, 
-            { name: "Microsoft Powerpoint", value: "85%" }, 
-            { name: "Adobe Photoshop", value: "70%" }, 
-        ], 
-        references: [ 
-            { 
-                name: "MICHAEL R.MAGEE", 
-                address: "4418 Bobcat Drive Southfield, MI 48034", 
-            }, 
-            { 
-                name: "TRAVIS M.GODINEZ", 
-                address: "2755 Oakmound Drive Chicago, IL 60605", 
-            } 
-        ], 
-        hoddies: [ 
-            {name: "READING BOOKS"}, 
-            {name: "PLAYING FOOTBALL"}, 
-            {name: "TRAVELING"}, 
-        ], 
-        about_me: [ 
-            {text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus ex ducimus odit qui incidunt dolorum eligendi explicabo mollitia! accusantium incidunt. Eius soluta molestiae dolore" }, 
-            {text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda beatae autem sunt at?"} 
-        ] 
-    }); 
-}); 
- 
-app.listen(port, () => { 
-    console.log(`Server works on http://localhost:${port}`); 
+import express from 'express';
+import cors from 'cors';
+const app = express();
+const port = 4000;
+
+app.use(cors());
+
+app.get('/data', (req, res) => {
+    res.json({
+        education: [
+            {
+                institution: "UNIVERSITY NAME",
+                years: "2018 - PRESENT",
+                degree: "MASTER OF CREATIVEARTS",
+                description: "Lorem ipsum dolor sit, amet consectetur<br> adipisicing elit. Fugiat, molestias provident."
+            },
+            {
+                institution: "UNIVERSITY NAME",
+                years: "2015 - 2017",
+                degree: "MASTER GRAPHIC & WEB DESIGHER",
+                description: "Lorem ipsum dolor sit, amet consectetur<br> adipisicing elit. Fugiat, molestias provident."
+            },
+            {
+                institution: "UNIVERSITY NAME",
+                years: "2010 - 2012",
+                degree: "MASTER OF DESIGH",
+                description: "Lorem ipsum dolor sit, amet consectetur<br> adipisicing elit. Fugiat, molestias provident."
+            } ],
+        jobList: [
+            {
+                company: "COMPNY NAME",
+                years: "2020 - Present",
+                position: "WEB DESINGER",
+                description: "Lorem ipsum dolor sit, amet consectetur<br> adipisicing elit. Fugiat, molestias provident."
+            },
+            {
+                company: "COMPNY NAME",
+                years: "2015 - 2018",
+                position: "Graphic Designer",
+                description: "Lorem ipsum dolor sit, amet consectetur<br> adipisicing elit. Fugiat, molestias provident."
+            },
+            {
+                company: "COMPNY NAME",
+                years: "2010 - 2015",
+                position: "MARKETING MANAGER",
+                description: "Lorem ipsum dolor sit, amet consectetur<br> adipisicing elit. Fugiat, molestias provident."
+            } ],
+        contact: [
+            {
+                phone: "+1-856-822-2156",
+                website: "www.yourwebsite.com yourinfo@email.com",
+                address: "1173 Valley Street Camber, NJ 08102"
+            }],
+        skills: [
+            { name: "Microsoft Word", level: "90%" },
+            { name: "Adobe illustrator", level: "65%" },
+            { name: "Microsoft Powerpoint", level: "85%" },
+            { name: "Adobe Photoshop", level: "70%" },
+        ],
+        references: [
+            {
+                name: "MICHAEL R.MAGEE",
+                address: "4418 Bobcat Drive Southfield, MI 48034",
+            },
+            {
+                name: "TRAVIS M.GODINEZ",
+                address: "2755 Oakmound Drive Chicago, IL 60605",
+            }
+        ],
+        hobbies: [
+            {name: "READING BOOKS"},
+            {name: "PLAYING FOOTBALL"},
+            {name: "TRAVELING"},
+        ],
+        about_me: [
+            {text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus ex ducimus odit qui incidunt dolorum eligendi explicabo mollitia! accusantium incidunt. Eius soluta molestiae dolore" },
+            {text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda beatae autem sunt at?"}
+        ]
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server works on http://localhost:${port}`);
 });
